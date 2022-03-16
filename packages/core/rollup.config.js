787e7dd2b9f5d76283c1782d0c81fc4550915436
Jonas Wanner <jonas.wanner@clavisit.com>
2022-03-16 22:52:56 +0100

    feat: add rollup process to bundle code with sourcemaps and types

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,22 +1,32 @@
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import del from 'rollup-plugin-delete'
+import dts from 'rollup-plugin-dts'
 
-
-export default {
+export default [
+  {
     input: 'src/mod.ts',
-    output: [
-        {
-            file: packageJson.main,
-            format: 'cjs',
-            sourcemap: true,
-            name: 'react-lib'
-        },
-        {
-            file: packageJson.module,
-            format: 'esm',
-            sourcemap: true
-        }
-    ],
     plugins: [
-        commonjs(),
-        typescript({ tsconfig: '../../tsconfig.json' }),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json', outputToFilesystem: true })
+    ],
+    output: [
+      {
+        file: 'dist/mod.js',
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    external: ['jotai', 'react']
+  },
+  {
+    input: 'dist/types/mod.d.ts',
+    plugins: [dts(), del({ targets: ['dist/types'], hook: 'buildEnd' })],
+    output: [
+      {
+        file: 'dist/mod.d.ts',
+        format: 'esm'
+      }
     ]
-}
+  }
+]
