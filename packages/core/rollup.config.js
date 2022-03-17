82577c793f87c8ac46b94fdcfe0f56d6bd7a2e85
Jonas Wanner <jonas.wanner@clavisit.com>
2022-03-17 07:21:43 +0100

    feat: add dev command to workspace

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -3,6 +3,8 @@ import typescript from '@rollup/plugin-typescript'
 import del from 'rollup-plugin-delete'
 import dts from 'rollup-plugin-dts'
 
+const production = !process.env.ROLLUP_WATCH
+
 export default [
   {
     input: 'src/mod.ts',
@@ -21,7 +23,10 @@ export default [
   },
   {
     input: 'dist/types/mod.d.ts',
-    plugins: [dts(), del({ targets: ['dist/types'], hook: 'buildEnd' })],
+    plugins: [
+      dts(),
+      production && del({ targets: ['dist/types'], hook: 'buildEnd' })
+    ],
     output: [
       {
         file: 'dist/mod.d.ts',
