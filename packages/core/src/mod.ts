eb9af247494212534e51afbb1c2c7afc5e0b0ba2
Jonas Wanner <jonas.wanner@clavisit.com>
2022-03-21 10:34:21 +0100

    feat: add new components, hooks and types to mod.ts

--- packages/core/src/mod.ts
+++ packages/core/src/mod.ts
@@ -1,5 +1,13 @@
-import { OneProvider } from './components/mod'
+import { OnePageResponse } from '@/types/OnePage.types'
+import { OneEngine, OneProvider } from './components/mod'
 import './css/main.css'
-import { useIsEditMode } from './hooks/mod'
+import { useIsEditMode, useLogin, useLogout } from './hooks/mod'
 
-export { OneProvider, useIsEditMode }
+export {
+  OneProvider,
+  OneEngine,
+  useIsEditMode,
+  useLogout,
+  useLogin,
+  OnePageResponse
+}
