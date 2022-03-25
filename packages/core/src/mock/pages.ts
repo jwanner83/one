fd4cd35f4b3ce5d494b3405027d60589cc6bf47a
Jonas Wanner <jonas.wanner@clavisit.com>
2022-03-25 21:38:30 +0100

    feat: add mock post and pages

---
+++ packages/core/src/mock/pages.ts
@@ -0,0 +1,29 @@
+import { LatestItemElement } from '@/components/interface/config/latest/LatestItems.types'
+
+export const pages: LatestItemElement[] = [
+  {
+    name: 'Über uns',
+    changed: new Date(),
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+  },
+  {
+    name: 'Kontakt',
+    changed: new Date(),
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+  },
+  {
+    name: 'Home',
+    changed: new Date(),
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+  },
+  {
+    name: 'Anfahrt',
+    changed: new Date(),
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+  },
+  {
+    name: 'Podcast',
+    changed: new Date(),
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+  }
+]
---
+++ packages/core/src/mock/posts.ts
@@ -0,0 +1,34 @@
+import { LatestItemElement } from '@/components/interface/config/latest/LatestItems.types'
+
+export const posts: LatestItemElement[] = [
+  {
+    name: 'Das Versteck',
+    type: 'podcast',
+    changed: new Date(),
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+  },
+  {
+    name: 'Die Zusammenfassung',
+    type: 'blog',
+    changed: new Date(),
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+  },
+  {
+    name: 'Wie alles begann',
+    type: 'podcast',
+    changed: new Date(),
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+  },
+  {
+    name: 'Auf ein neues Jahr',
+    type: 'blog',
+    changed: new Date(),
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+  },
+  {
+    name: 'Im Archiv',
+    type: 'podcast',
+    changed: new Date(),
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+  }
+]
