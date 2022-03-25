710baf39f91899041aa464d47f069703b3c2722c
Jonas Wanner <jonas.wanner@clavisit.com>
2022-03-25 21:37:11 +0100

    feat: include menu button in OneInterfaceHeader.tsx which triggers config mode atom

--- packages/core/src/components/interface/header/OneInterfaceHeader.tsx
+++ packages/core/src/components/interface/header/OneInterfaceHeader.tsx
@@ -1,15 +1,22 @@
 import Logo from '@/components/icons/Logo'
 import Menu from '@/components/icons/Menu'
+import { configModeAtom } from '@/state/atoms'
+import { useAtom } from 'jotai'
 import React from 'react'
 
 export default function OneInterfaceHeader() {
+  const [configMode, setConfigMode] = useAtom(configModeAtom)
+
   return (
-    <div className="px-5 py-5 mb-4 bg-gray-100 shadow-2xl rounded-2xl flex justify-between items-center">
+    <div className="px-5 py-5 mb-4 bg-gray-100 shadow-2xl rounded-2xl flex justify-between items-center relative z-10">
       <div className="h-10 w-10 ">
         <Logo />
       </div>
       <div>
-        <div className="h-8 w-8">
+        <div
+          className="h-8 w-8 rounded p-0.5 mr-1 cursor-pointer transition hover:bg-gray-200 active:scale-90"
+          onClick={() => setConfigMode(!configMode)}
+        >
           <Menu />
         </div>
       </div>
