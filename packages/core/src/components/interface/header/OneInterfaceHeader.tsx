c0cb714023523108ce614a92d9132e1e840d97fd
Jonas Wanner <jonas.wanner@clavisit.com>
2022-03-25 20:19:58 +0100

    feat: include icon components inside of interface header

--- packages/core/src/components/interface/header/OneInterfaceHeader.tsx
+++ packages/core/src/components/interface/header/OneInterfaceHeader.tsx
@@ -1,7 +1,18 @@
+import Logo from '@/components/icons/Logo'
+import Menu from '@/components/icons/Menu'
 import React from 'react'
 
 export default function OneInterfaceHeader() {
   return (
-    <div className="px-6 py-6 mb-4 bg-gray-100 shadow-2xl rounded-2xl">one</div>
+    <div className="px-5 py-5 mb-4 bg-gray-100 shadow-2xl rounded-2xl flex justify-between items-center">
+      <div className="h-10 w-10 ">
+        <Logo />
+      </div>
+      <div>
+        <div className="h-8 w-8">
+          <Menu />
+        </div>
+      </div>
+    </div>
   )
 }
