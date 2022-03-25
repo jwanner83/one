c47bf4b2a9bef0b1cfe6c73e43f082ae5d49d31c
Jonas Wanner <jonas.wanner@clavisit.com>
2022-03-25 21:38:20 +0100

    feat: add OneInterfaceConfig.tsx as soon as config mode is true

--- packages/core/src/components/interface/OneInterface.tsx
+++ packages/core/src/components/interface/OneInterface.tsx
@@ -1,4 +1,7 @@
+import OneInterfaceConfig from '@/components/interface/config/OneInterfaceConfig'
 import OneInterfaceHeader from '@/components/interface/header/OneInterfaceHeader'
+import { configModeAtom } from '@/state/atoms'
+import { useAtomValue } from 'jotai'
 import React, { ReactNode } from 'react'
 
 type OneInterfaceParmas = {
@@ -6,13 +9,18 @@ type OneInterfaceParmas = {
 }
 
 export default function OneInterface({ children }: OneInterfaceParmas) {
+  const configMode = useAtomValue(configModeAtom)
+
   return (
     <div className="h-screen relative">
       <div className="h-full grid grid-rows-one-layout p-8">
         <div>
           <OneInterfaceHeader />
         </div>
-        <div className="rounded-2xl overflow-x-auto">{children}</div>
+        <div className="rounded-2xl overflow-x-auto relative">
+          {configMode && <OneInterfaceConfig />}
+          {children}
+        </div>
       </div>
     </div>
   )
