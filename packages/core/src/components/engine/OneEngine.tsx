3c8c214c2e31bf89e4fd7d072b168950a751aebb
Jonas Wanner <jonas.wanner@clavisit.com>
2022-03-28 19:29:25 +0200

    feat: change default value of one engine to lorem ipsum and remove title

--- packages/core/src/components/engine/OneEngine.tsx
+++ packages/core/src/components/engine/OneEngine.tsx
@@ -15,8 +15,32 @@ export default function OneEngine({ page }: OneEngineParams) {
 
   return (
     <div>
-      <h1>{page?.title}</h1>
-      <main>{editMode ? <ReactEditorJS /> : <p>content</p>}</main>
+      <main>
+        {editMode ? (
+          <ReactEditorJS />
+        ) : (
+          <>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p className="mt-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </>
+        )}
+      </main>
     </div>
   )
 }
