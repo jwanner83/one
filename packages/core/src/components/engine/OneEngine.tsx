d07e884947b9139f007686f1f79684cb9024973f
Jonas Wanner <jonas.wanner@clavisit.com>
2022-03-28 13:34:53 +0200

    feat: add react editor js to one engine

--- packages/core/src/components/engine/OneEngine.tsx
+++ packages/core/src/components/engine/OneEngine.tsx
@@ -1,10 +1,22 @@
+import { editModeAtom } from '@/state/atoms'
 import { OnePageResponse } from '@/types/OnePage.types'
+import { useAtomValue } from 'jotai'
 import React from 'react'
+import { createReactEditorJS } from 'react-editor-js'
+
+const ReactEditorJS = createReactEditorJS()
 
 type OneEngineParams = {
   page: OnePageResponse
 }
 
 export default function OneEngine({ page }: OneEngineParams) {
-  return <h1>{page?.title}</h1>
+  const editMode = useAtomValue(editModeAtom)
+
+  return (
+    <div>
+      <h1>{page?.title}</h1>
+      <main>{editMode ? <ReactEditorJS /> : <p>content</p>}</main>
+    </div>
+  )
 }
