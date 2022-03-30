bb50cf6e198c5f109cb3a6029d18ef18097ab06f
Jonas Wanner <jonas.wanner@clavisit.com>
2022-03-30 11:33:54 +0200

    feat: change default value of editor js to lorem ipsum

--- packages/core/src/components/engine/OneEngine.tsx
+++ packages/core/src/components/engine/OneEngine.tsx
@@ -17,7 +17,41 @@ export default function OneEngine({ page }: OneEngineParams) {
     <div>
       <main>
         {editMode ? (
-          <ReactEditorJS />
+          <ReactEditorJS
+            onChange={console.log}
+            defaultValue={{
+              time: 1648632050499,
+              blocks: [
+                {
+                  id: 'pmzWrBGAQb',
+                  type: 'paragraph',
+                  data: {
+                    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.`
+                  }
+                },
+                {
+                  id: 'pmzWrBGAQb',
+                  type: 'paragraph',
+                  data: {
+                    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.`
+                  }
+                }
+              ],
+              version: '2.23.1'
+            }}
+          />
         ) : (
           <>
             <p>
