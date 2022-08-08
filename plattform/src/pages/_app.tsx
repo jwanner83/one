06940b1ceecbbf5b253fb826d562c968602492db
Jonas Wanner <jonas.wanner@clavisit.com>
2022-08-08 13:04:32 +0200

    feat: adding icon and title to plattform

--- plattform/src/pages/_app.tsx
+++ plattform/src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <title>one - the all in one solution</title>
+        <link rel="icon" type="image/x-icon" href="/icon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
   )
 }
