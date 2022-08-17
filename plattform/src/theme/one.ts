caafc5eee9716197a2a8a54771a72ddec5b00b63
Jonas Wanner <jonas.wanner@clavisit.com>
2022-08-17 14:53:17 +0200

    feat: customizing theme

--- plattform/src/theme/one.ts
+++ plattform/src/theme/one.ts
@@ -1,9 +1,38 @@
-import { createTheme } from '@mui/material'
+import { createTheme, Shadows } from '@mui/material'
 
 export const theme = createTheme({
   palette: {
+    background: {
+      default: '#dee1e7',
+      paper: '#f6f7ff'
+    },
     primary: {
-      main: '#1c38c7'
+      main: '#a1e598'
+    },
+    secondary: {
+      main: '#000'
+    }
+  },
+  shadows: Array(25).fill('none') as Shadows,
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+          borderRadius: '8px',
+          padding: '6px 60px 5px',
+          fontWeight: 'bold',
+          transition: '200ms',
+          ':active': {
+            transform: 'scale(0.95)'
+          }
+        }
+      }
     }
   }
 })
